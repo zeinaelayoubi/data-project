8bf93bc7822ba38243315d6bde5726c41ffdc49c
zeinaelayoubi <zeina.elayoubi@lau.edu>
2022-11-30 16:44:16 +0200

    Fix

--- coding_projects/src/components/Billing.jsx
+++ coding_projects/src/components/Billing.jsx
@@ -14,8 +14,8 @@ const Billing = () => (
 
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-        Easily control your <br className="sm:block hidden" /> billing &
-        invoicing
+        Easily pay for your<br className="sm:block hidden" /> custom bike!
+        
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
